615aaa4425de84a9364c531e463f3facf2e544b4
shelly trigg <shellywanamaker@gmail.com>
2018-10-25 10:29:29 -0700

    mox

--- methylation_analysis/20181011_bmrkAS1.2I60test_Pgenr.sh
+++ methylation_analysis/20181011_bmrkAS1.2I60test_Pgenr.sh
@@ -22,7 +22,7 @@
 %%bash
 
 find /gscratch/srlab/strigg/data/Pgenr/FASTQS/EPI-*R1* \
-| xargs basename -s _001_val_1.fq.gz | xargs -I{} /gscratch/srlab/programs/Bismark-0.19.0/bismark \
+| xargs basename -s _R1_001_val_1.fq.gz | xargs -I{} /gscratch/srlab/programs/Bismark-0.19.0/bismark \
 --path_to_bowtie /gscratch/srlab/programs/bowtie2-2.3.4.1-linux-x86_64 \
 --samtools_path /gscratch/srlab/programs/samtools-1.9 \
 --score_min L,0,-1.2 \
@@ -30,8 +30,8 @@ find /gscratch/srlab/strigg/data/Pgenr/FASTQS/EPI-*R1* \
 -u 10000 \
 --non_directional \
 --genome /gscratch/srlab/strigg/data/Pgenr/ \
--1 /gscratch/srlab/strigg/data/Pgenr/FASTQS/{}_001_val_1.fq.gz \
--2 /gscratch/srlab/strigg/data/Pgenr/FASTQS/{}_001_val_2.fq.gz \
+-1 /gscratch/srlab/strigg/data/Pgenr/FASTQS/{}_R1_001_val_1.fq.gz \
+-2 /gscratch/srlab/strigg/data/Pgenr/FASTQS/{}_R2_001_val_2.fq.gz \
 -o /gscratch/srlab/strigg/analyses/20181011/ 
 
 
@@ -47,7 +47,8 @@ find /gscratch/srlab/strigg/data/Pgenr/FASTQS/EPI-*R1* \
 #create summary report
 cat /gscratch/srlab/strigg/analyses/20181011/*PE_report.txt | \
 grep 'Mapping\ efficiency\:' | \
-cat - /gscratch/srlab/strigg/analyses/20181011/*.deduplication_report.txt > /gscratch/srlab/strigg/analyses/20181011/EPI_mapping_dedup_summary.txt
+cat - /gscratch/srlab/strigg/analyses/20181011/*.deduplication_report.txt \
+grep 'Mapping\ efficiency\:\|removed' > /gscratch/srlab/strigg/analyses/20181011/EPI_mapping_dedup_summary.txt
 #clean up summary report
 sed 's/Mapping\ efficiency\://g' /gscratch/srlab/strigg/analyses/20181011/EPI_mapping_dedup_summary.txt | \
 sed 's/Total\ number\ duplicated\ alignments\ removed\://g' | \
